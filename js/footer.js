159d2ca7c3acb6f664a8ae84970d274203d53dfb
sanduleonard31 <sanduleonard21@stud.ase.ro>
2025-10-23 01:59:24 +0300

    Enhance layout and structure: add header and footer HTML, style adjustments, and background image

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var footerElement = document.getElementById('footer');
+    fetch('../footer.html')
+        .then(function(response) { return response.text(); })
+        .then(function(html) { footerElement.innerHTML = html; });
+});
---
+++ js/header.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var headerElement = document.getElementById('header');
+    fetch('../header.html')
+        .then(function(response) { return response.text(); })
+        .then(function(html) { headerElement.innerHTML = html; });
+});
