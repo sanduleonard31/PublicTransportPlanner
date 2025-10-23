4a1af443335c1b8dfc84fe6ea399c5ae7230780e
sanduleonard31 <sanduleonard21@stud.ase.ro>
2025-10-24 01:33:58 +0300

    Fix fetch paths in header and footer scripts

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var footerElement = document.getElementById('footer');
-    fetch('f./ooter.html')
+    fetch('footer.html')
         .then(function(response) { return response.text(); })
         .then(function(html) { footerElement.innerHTML = html; });
 });
